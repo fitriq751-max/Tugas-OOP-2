9d836cdc0478e44d5faef76fa02b1504e4b3193a
fitriq751-max <fitriq751@gmail.com>
2026-02-22 21:32:33 +0700

    pengguna.py

--- pengguna.py
+++ pengguna.py
@@ -1,3 +1,4 @@
+# A Member class to represent a library user
 class Member:
     def __init__(self, name, member_id):
         self.name = name
@@ -7,4 +8,5 @@ class Member:
 class Staff:
     def __init__(self, name, staff_id):
         self.name = name
+
         self.staff_id = staff_id
